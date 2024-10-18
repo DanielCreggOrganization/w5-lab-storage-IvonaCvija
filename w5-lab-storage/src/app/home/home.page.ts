7a32c65af0bc4539bb6c30ade8c8803c28acc429
Ivona Cvija <123738999+IvonaCvija@users.noreply.github.com>
2024-10-18 13:02:46 +0000

    remove, clear, find keys, count items methods added

--- w5-lab-storage/src/app/home/home.page.ts
+++ w5-lab-storage/src/app/home/home.page.ts
@@ -38,4 +38,44 @@ export class HomePage {
       this.output = `Error getting item: ${error}`;
     }
   }
+
+  async removeItem(){
+    try {
+      const value = await this.storage.remove(this.key);
+      this.output = `Remove ${this.key}: ${value}`;
+    } catch (error) {
+      console.error('Error removing item', error);
+      this.output = `Error removing item: ${error}`;
+    }
+  }
+
+  async clearAll(){
+    try {
+      const value = await this.storage.clear();;
+      this.output = `Clear all ${this.key}: ${value}`;
+    } catch (error) {
+      console.error('Error clearing all items', error);
+      this.output = `Error clearing all items: ${error}`;
+    }
+  }
+
+  async retrieveAllKeys(){
+    try {
+      const keys = await this.storage.keys();
+      this.output = `Retrieve ${this.key}: ${keys}`;
+    } catch (error) {
+      console.error('Error retrieving all keys', error);
+      this.output = `Error retrieving all keys: ${error}`;
+    }
+  }
+
+  async getNumberOfItems(){
+    try {
+      const length = await this.storage.length();
+      this.output = `Remove ${this.key}: ${length}`;
+    } catch (error) {
+      console.error('Error getting number of items', error);
+      this.output = `Error getting number of items: ${error}`;
+    }
+  }
 }
